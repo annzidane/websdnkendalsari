9e5457beb03b94b5c46b0ab6e766fcd9650776e8
20102028@ittelkom-pwt.ac.id <annzidane>
2022-10-18 21:47:49 +0700

    first commit

---
+++ application/views/depan/v_contact.php
@@ -0,0 +1,25 @@
+
+<section class="contact-section">
+            <div class="container">
+                <div class="d-none d-sm-block mb-5 pb-4">
+                    
+                                        
+                    
+                    <div class="row">
+                    <div class="col-md-12">
+                        
+                    <iframe src="https://maps.google.com/maps?q=sd%20n%2006%20kendalsari&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" style="width: 100%; height: 100%; position: relative" id="fileframe" allowfullscreen></iframe>
+                        
+                        <iframe style="width: 100vw;height: 100vh;position: relative;" src="" frameborder="0" allowfullscreen></iframe>
+                        
+                    
+                    </div>
+                    </div>
+                    
+    
+                </div>
+    
+    
+                
+            </div>
+        </section>
---
+++ application/views/theme/header.php
@@ -0,0 +1,18 @@
+
+            <div class="header-top_area">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="header_top_wrap d-flex justify-content-between align-items-center">
+                                <div class="text_wrap">
+                                    <p><span>Call : +62823-2734-7560</span> | <span>Email : sdnkendalsari06@gmail.com</span></p>
+                                </div>
+                                <div class="text_wrap">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        
